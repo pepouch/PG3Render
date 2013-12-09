adefe74f2b3d045477c46d127711fe290ad8b186
Petr Kratochvíl <petr-kratochvil@seznam.cz>
2013-12-09 21:59:13 +0100

    Fix: added multiplying by cosines on each scene hit

--- src/pathtracer_global.hxx
+++ src/pathtracer_global.hxx
@@ -47,7 +47,7 @@ public:
           sceneHitState.frame.SetFromZ(isect.normal);
           sceneHitState.wol = sceneHitState.frame.ToLocal(-ray.dir);
 
-          LoDirect += this->pathForward(sceneHitState);
+          LoDirect += this->pathForward(sceneHitState, 0);
         }
 
         mFramebuffer.AddColor(sample, LoDirect);
@@ -57,12 +57,23 @@ public:
     mIterations++;
   }
 
-  Vec3f pathForward(SceneHitState state)
+  float maxSum(Vec3f v, Vec3f w)
+  {
+    float max = v.x + w.x;
+    max = std::max(max, v.y + w.y);
+    max = std::max(max, v.z + w.z);
+    return max;
+  }
+
+  Vec3f pathForward(SceneHitState state, int depth)
   {
     float roulette = this->mRng.GetFloat();
-    float reflectance = state.mat.mDiffuseReflectance.Max() + state.mat.mPhongReflectance.Max();
+    float reflectance = 1.0f;//std::max(state.mat.mDiffuseReflectance.Max(), state.mat.mPhongReflectance.Max());
+    
     if (roulette > reflectance)
+    {
       return Vec3f(0);
+    }
 
     Vec2f randomVec = this->mRng.GetVec2f();
     float pdf = 0;
@@ -73,13 +84,17 @@ public:
     state.setRayFromSample(sampleHemisphere);
     Vec3f illum = this->sampleDirection(state);
 
-    if (state.light != NULL)
+    if (state.light != nullptr)
+    {
       return illum  * brdf / (pdf * reflectance);
+    }
+
+    float cosThetaOut = Dot(state.frame.mZ, state.sampledRay.dir);
 
     SceneHitState newState(mScene.GetMaterial(state.isect.matID));
     newState.surfPt = state.surfPt + state.sampledRay.dir * state.isect.dist;
     newState.frame.SetFromZ(state.isect.normal);
     newState.wol = newState.frame.ToLocal(-state.sampledRay.dir);
-    return this->pathForward(newState) * brdf / (pdf * reflectance);
+    return this->pathForward(newState, depth+1) * brdf / (pdf * reflectance) * cosThetaOut;
   }
 };
